dbe3152da8d353fdaccef15a8c4c51b9b34f396d
jameshy <jameshy64@gmail.com>
2015-03-05 23:06:17 +0000

    better implementation of BindingResultslist

--- wpfmenu/Types/BindingResultsList.cs
+++ wpfmenu/Types/BindingResultsList.cs
@@ -1,16 +1,39 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace wpfmenu.Types
 {
     /// <summary>
-    /// BindingList of <see cref="Model.Result"/>, but automatically increments Model.Result.Index (for xaml usage).
+    /// BindingList of <see cref="Model.Result" />, but automatically increments Model.Result.Index (for xaml usage).
+    // todo: add support for Add()
     /// </summary>
-    public class BindingResultsList : BindingList<Model.Result>
+    public class BindingResultsList : List<Model.Result>, INotifyCollectionChanged
     {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
         public new void Add(Model.Result value)
         {
-            value.Index = Count;
-            base.Add(value);
+            throw new NotImplementedException();
+            //value.Index = Count;
+            //base.Add(value);
+        }
+        public void NotifyChanged(NotifyCollectionChangedEventArgs e)
+        {
+            
+            if (CollectionChanged != null) {
+                var alternate = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+                CollectionChanged(this, alternate);
+            }
+        }
+        public void AddRange(List<Model.Result> collection)
+        {
+            int i = 0;
+            foreach (var r in collection) {
+                r.Index = Count + i++;
+            }
+            base.AddRange(collection);
+            NotifyChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, collection));
         }
     }
 }
