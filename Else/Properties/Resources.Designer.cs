569db6ba10c0c5058d6d46e9c62e70747283805b
jameshy <jameshy64@gmail.com>
2015-03-10 14:08:47 +0000

    Rename wpfmenu -> Else

--- Else/Properties/Resources.Designer.cs
+++ Else/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace wpfmenu.Properties {
+namespace Else.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace wpfmenu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("wpfmenu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Else.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
