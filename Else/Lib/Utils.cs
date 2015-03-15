ae4146ff31ab35a6434ef41811eaf526cd371f5b
jameshy <jameshy64@gmail.com>
2015-03-15 14:12:46 +0000

    Add Utils library class

---
+++ Else/Lib/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Else.Lib
+{
+    public static class Utils
+    {
+        public static void WasteTime(int seconds)
+        {
+            var endTime = DateTime.Now.AddSeconds(seconds);
+            while(true) {
+                if (DateTime.Now >= endTime) {
+                    break;
+                }
+            }
+        }
+    }
+}
