7f68743b20a44cd2016cd8fe98516088017e3d21
jameshy <jameshy64@gmail.com>
2015-03-18 14:20:18 +0000

    Filesystem: improve searching

--- Else/Core/Plugins/Filesystem.cs
+++ Else/Core/Plugins/Filesystem.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.OleDb;
 using System.Diagnostics;
-using System.Windows.Forms;
 using Else.Lib;
 using Else.Model;
 using Microsoft.Search.Interop;
+
 
 namespace Else.Core.Plugins
 {
@@ -17,6 +14,7 @@ namespace Else.Core.Plugins
         /// <summary>
         /// Plugin setup
         /// </summary>
+        private const string ConnectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
         ResultProvider _openProvider;
         public override void Setup()
         {
@@ -25,20 +23,27 @@ namespace Else.Core.Plugins
                 Query = (query, token) => {
                     if (query.HasArguments) {
                         // do search
-                        var results = new List<Result>();
-
-                        foreach (var f in Search_FromAQS(query.Arguments)) {
-                            results.Add(new Result{
-                                Title = f.ItemDisplayName,
-                                SubTitle = f.ItemPathDisplay,
-                                Launch = query1 => {
-                                    Process.Start(f.ItemUrl);
-                                }
-                            });
+                        try {
+                            var results = new List<Result>();
+                            foreach (var f in Search_FromAQS(query.Arguments)) {
+                                results.Add(new Result{
+                                    Title = f.ItemDisplayName + "  " + f.Rank,
+                                    SubTitle = f.ItemPathDisplay,
+                                    Launch = query1 => {
+                                        Process.Start(f.ItemUrl);
+                                    }
+                                });
+                            }
+                            return results;
                         }
-                        return results;
+                        catch (Exception) {
+                            return new Result{
+                                Title = "Query Error"
+                            }.ToList();
+                        }
+                        
                     }
-                    // otherwise the query has not been provided yet, running the action will autocomplete the query
+                    // otherwise the aqsQuery has not been provided yet, running the action will autocomplete the aqsQuery
                     return new Result{
                         Title = "Open file..",
                         Launch = query1 => PluginCommands.RewriteQuery(_openProvider.Keyword + ' ')
@@ -49,17 +54,20 @@ namespace Else.Core.Plugins
             Providers.Add(_openProvider);
         }
         
-        public class SearchResult {
+        class SearchResult {
             public string ItemDisplayName;
             public string ItemPathDisplay;
             public string ItemUrl;
+            public int Rank;
         }
 
+        /// <summary>
+        /// Queries the windows search index using simple keyword matching on the FileName and ItemDisplayName.
+        /// </summary>
+        /// <param name="keywords">The keywords.</param>
+        /// <returns>An iterator of SearchResults</returns>
         private static IEnumerable<SearchResult> Search_FromSQL(string keywords)
         {
-            //if (!keywords.EndsWith("*")) {
-            //    keywords += "*";
-            //}
             var tostrip = new List<string>{
                 "\\",
                 "/",
@@ -86,35 +94,48 @@ namespace Else.Core.Plugins
             return Search(sql);
 
         }
-        private static IEnumerable<SearchResult> Search_FromAQS(string query)
+        
+        /// <summary>
+        /// Queries the windows search index from an AQS query.
+        /// </summary>
+        /// <remarks><see cref="https://msdn.microsoft.com/en-us/library/aa965711%28v=vs.85%29.aspx"/></remarks>
+        /// <param name="aqsQuery">The AQS query.</param>
+        /// <returns>An iterator of SearchResults</returns>
+        private static IEnumerable<SearchResult> Search_FromAQS(string aqsQuery)
         {
             var manager = new CSearchManager();
             var catalogManager = manager.GetCatalog("SystemIndex");
             var queryHelper = catalogManager.GetQueryHelper();
-            queryHelper.QuerySelectColumns = "System.ItemNameDisplay, System.ItemPathDisplay, System.ItemUrl";
-            queryHelper.QueryWhereRestrictions = "AND scope='file:' AND System.FileAttributes <> ALL BITWISE 2";
+            queryHelper.QuerySelectColumns = "System.ItemNameDisplay, System.ItemPathDisplay, System.ItemUrl, System.Search.Rank";
+            //queryHelper.QueryWhereRestrictions = "AND scope='file:' AND System.FileAttributes <> ALL BITWISE 2";
             queryHelper.QueryContentProperties = "System.ItemNameDisplay";
-            queryHelper.QueryMaxResults = 10;
+            queryHelper.QueryMaxResults = 100;
             queryHelper.QuerySorting = "System.Search.Rank DESC";
-            var sql = queryHelper.GenerateSQLFromUserQuery(query);
+            var sql = queryHelper.GenerateSQLFromUserQuery(aqsQuery);
             return Search(sql);
         }
-        
 
-        private const string ConnectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
+        /// <summary>
+        /// Sends a raw SQL query to the windows search index.
+        /// </summary>
+        /// <param name="sql">The SQL.</param>
+        /// <returns>An iterator of SearchResults</returns>
         private static IEnumerable<SearchResult> Search(string sql)
         {
-            Debug.Print("SQL = [{0}]", sql);
+            //Debug.Print("SQL = [{0}]", sql);
             var command = new OleDbCommand(sql);
             var connection = new OleDbConnection(ConnectionString);
             OleDbDataReader reader;
             
             try {
+                // connect
                 connection.Open();
+                // execute the command
                 command.Connection = connection;
                 reader = command.ExecuteReader();
             }
             catch (OleDbException e) {
+                // query resulted in an error
                 Debug.Print("Got OleDbException, error code is 0x{0:X}L", e.ErrorCode);
                 Debug.Print("Exception details:");
                 for (var i = 0; i < e.Errors.Count; i++) {
@@ -128,11 +149,13 @@ namespace Else.Core.Plugins
                 throw;
             }
             if (reader != null) {
+                // return successful results
                 while (reader.Read()) {
                     var result = new SearchResult{
                         ItemDisplayName = reader.GetString(0),
                         ItemPathDisplay = reader.GetString(1),
-                        ItemUrl = reader.GetString(2)
+                        ItemUrl = reader.GetString(2),
+                        Rank = reader.GetInt32(3)
                     };
                     yield return result;
                 }
