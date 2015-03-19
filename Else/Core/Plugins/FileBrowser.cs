d7430b408f20e8a3a35f02fe70e4a3a247038a97
jameshy <jameshy64@gmail.com>
2015-03-19 23:38:53 +0000

    FileBrowser: handle unauthorized access exception

--- Else/Core/Plugins/FileBrowser.cs
+++ Else/Core/Plugins/FileBrowser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -67,40 +68,47 @@ namespace Else.Core.Plugins
                                     });
                                 }
                             });
+                            try {
+                                foreach (var dir in Directory.EnumerateDirectories(dirName)) {
+                                    filterEntry(dir, true);
+                                } 
+                                foreach (var file in Directory.EnumerateFiles(dirName)) {
+                                    filterEntry(file, false);
+                                }
+                                IEnumerable<FileSystemEntry> sorted;
+                                if (ListDirectoriesFirst) {
+                                    sorted = entries.OrderByDescending(e => e.IsDirectory).ThenBy(e => e.FileName);
+                                }
+                                else {
+                                    sorted = entries.OrderBy(e => e.FileName);
+                                }
 
-                            foreach (var dir in Directory.EnumerateDirectories(dirName)) {
-                                filterEntry(dir, true);
-                            } 
-                            foreach (var file in Directory.EnumerateFiles(dirName)) {
-                                filterEntry(file, false);
-                            }
-                            
-                            IEnumerable<FileSystemEntry> sorted;
-                            if (ListDirectoriesFirst) {
-                                sorted = entries.OrderByDescending(e => e.IsDirectory).ThenBy(e => e.FileName);
-                            }
-                            else {
-                                sorted = entries.OrderBy(e => e.FileName);
-                            }
-
-                            foreach (var item in sorted) {
-                                results.Add(new Result{
-                                    Title = item.FileName,
-                                    Launch = query1 => {
+                                foreach (var item in sorted) {
+                                    results.Add(new Result{
+                                        Title = item.FileName,
+                                        Launch = query1 => {
                                         
-                                        if (item.IsDirectory) {
-                                            // rewrite query to navigate to the selected directory
-                                            PluginCommands.RewriteQuery(item.Path + "\\");
-                                        }
-                                        else {
-                                            // launch the file (could be exe, jpeg, anything)
-                                            Process.Start(item.Path);
-                                        }
-                                    },
-                                    Icon = IconTools.GetBitmapForFile(item.Path)
-                                });
+                                            if (item.IsDirectory) {
+                                                // rewrite query to navigate to the selected directory
+                                                PluginCommands.RewriteQuery(item.Path + "\\");
+                                            }
+                                            else {
+                                                // launch the file (could be exe, jpeg, anything)
+                                                Process.Start(item.Path);
+                                            }
+                                        },
+                                        Icon = IconTools.GetBitmapForFile(item.Path)
+                                    });
+                                }
+                                return results;
                             }
-                            return results;
+                            // add more exceptions here?
+                            catch(UnauthorizedAccessException) {
+                                // improve
+                                return new Result{
+                                    Title="access denied"
+                                }.ToList();
+                            }
                         }
                     }
                     return new List<Result>();
