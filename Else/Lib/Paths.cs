2191f6ccb66cd5d74fad43bc9acd13c49778fee6
jameshy <jameshy64@gmail.com>
2015-03-27 20:10:36 +0000

    Add Lib.Paths for assisting with determining local + application data paths

---
+++ Else/Lib/Paths.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace Else.Lib
+{
+    /// <summary>
+    /// Handles resource paths for the entire app.
+    /// </summary>
+    public static class Paths
+    {
+        /// <summary>
+        /// The users custom data path (%appdata%\Else)
+        /// </summary>
+        public static string UserDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Assembly.GetEntryAssembly().GetName().Name);
+
+        /// <summary>
+        /// The directory that contains the application exe (c:\program files\Else)
+        /// </summary>
+        public static string AppDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        /// <summary>
+        /// Ensures the user directory and sub directories exist, otherwise creates them
+        /// </summary>
+        public static void CreateUserDirectories()
+        {
+            Directory.CreateDirectory(Path.Combine(UserDirectory, "Plugins"));
+            Directory.CreateDirectory(Path.Combine(UserDirectory, "Themes"));
+        }
+
+        public static string GetUserPath(string path)
+        {
+            return Path.Combine(UserDirectory, path);
+        }
+        public static string GetAppPath(string path)
+        {
+            return Path.Combine(AppDirectory, path);
+        }
+    }
+}
