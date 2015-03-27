f2367cd9a02a97a7cb3b1b559948682ed7bb3d3d
jameshy <jameshy64@gmail.com>
2015-03-27 20:05:17 +0000

    Add IPickerWindow and implementation ColorPicker

---
+++ Else/Controls/ColorPicker.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using Else.Interfaces;
+
+
+// not very confident about this stuff
+// the objective is to have ColorPicker implement IPickerWindow
+// so that we can later switchout ColorPicker with MockColorPicker
+namespace Else.Controls
+{
+    /// <summary>
+    /// Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker
+    {
+        public ColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Else/Interfaces/IPickerWindow.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Else.Interfaces
+{
+    public interface IPickerWindow {
+        RoutedPropertyChangedEventHandler<object> PropertyChanged { get; set; }
+        void Show(DependencyObject owner, string title);
+        void Close();
+    }
+}
