6041204ffed23432d4eafd22ec1450c196a2cfcb
jameshy <jameshy64@gmail.com>
2015-03-27 20:12:06 +0000

    Add Theme model and ThemeManager class

---
+++ Else/Lib/Theme.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using Newtonsoft.Json;
+
+namespace Else.Lib
+{
+    public class Theme {
+
+        /// <summary>
+        /// Failed to parse the theme JSON (either bad json, or missing required fields)
+        /// </summary>
+        public class ParseException : Exception {
+            public ParseException(string message) : base(message) {}
+        };
+
+        /// <summary>
+        /// Config for this theme (colors, fonts etc) is always stored in this dictionary.  We always work directly on this object when editing or reading theme config.
+        /// </summary>
+        public Dictionary<string, string> Config = new Dictionary<string, string>();
+        public string FilePath;
+
+        /// <summary>
+        /// Theme is editable and removable. (default themes are not editable)
+        /// </summary>
+        private bool _editable = true;
+
+        /// <summary>
+        /// Theme is editable and removable. (default themes are not editable)
+        /// </summary>
+        public bool Editable
+        {
+            get { return _editable; }
+            set { _editable = value; }
+        }
+
+
+        public string Name {
+            get {
+                return Config["Name"];
+            }
+            set {
+                Config["Name"] = value;
+            }
+        }
+        public string Author {
+            get {
+                return "by " + Config["Author"];
+            }
+            set {
+                Config["Author"] = value;
+            }
+        }
+        public string GUID {
+            get {
+                if (Config.ContainsKey("GUID")) {
+                    return Config["GUID"];
+                }
+                return Path.GetFileNameWithoutExtension(FilePath);
+            }
+            set {
+                Config["GUID"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Generates a new GUID for this theme, and also a new path based on that GUID.
+        /// </summary>
+        public void SetupNew()
+        {
+            GUID = Guid.NewGuid().ToString();
+            FilePath = Paths.GetUserPath(String.Format("themes/{0}.json", GUID));
+        }
+
+        /// <summary>
+        /// Loads the theme from a provided path, a .json file is expected.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="FileNotFoundException">theme file not found</exception>
+        public void LoadFromPath(string path)
+        {
+            if (!File.Exists(path)) {
+                throw new FileNotFoundException("theme file not found", path);
+            }
+            try {
+                dynamic result = JsonConvert.DeserializeObject(File.ReadAllText(path));
+                Dictionary<string, string> config = result.ToObject<Dictionary<string, string>>();
+                Load(config);
+                FilePath = path;
+                Debug.Print("Loaded theme: name={0} guid={1}", FilePath, GUID);
+            }
+            catch (ParseException e) {
+                Debug.Print("failed to parse theme file: {0} ~ {1}", path, e.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Loads the specified configuration.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        /// <exception cref="Else.Lib.Theme.ParseException">
+        /// Theme has no 'Name' field
+        /// or
+        /// Theme has no 'Author' field
+        /// </exception>
+        public void Load(Dictionary<string, string> config)
+        {
+            // Name is required
+            if (!config.ContainsKey("Name")) {
+                throw new ParseException("Theme has no 'Name' field");
+            }
+            // Author is required
+            if (!config.ContainsKey("Author")) {
+                throw new ParseException("Theme has no 'Author' field");
+            }
+            Config = config;
+        }
+
+        /// <summary>
+        /// Saves this theme to its FilePath.
+        /// </summary>
+        /// <param name="exportPath">Override destination path (e.g. when exporting).</param>
+        public void Save(string exportPath=null)
+        {
+            var json = JsonConvert.SerializeObject(Config, Formatting.Indented);
+            var path = exportPath ?? FilePath;
+            File.WriteAllText(path, json);
+            Debug.Print("saved {0} bytes to {1}", FilePath, path);
+        }
+
+        /// <summary>
+        /// Remove this themes file.
+        /// </summary>
+        public void Delete()
+        {
+            if (File.Exists(FilePath)) {
+                File.Delete(FilePath);
+            }
+        }
+
+        /// <summary>
+        /// Color paramaters we can take from the theme config and apply as brush overrides of our Styles.xaml
+        /// </summary>
+        private static string[] _colorParams = {
+            "WindowBorderColor",
+            "WindowBackgroundColor",
+            "QueryBoxBackgroundColor",
+            "QueryBoxTextColor",
+            "ResultBackgroundColor",
+            "ResultSelectedBackgroundColor",
+            "ResultTitleColor",
+            "ResultSubTitleColor",
+            "ResultSeparatorColor"
+        };
+
+        /// <summary>
+        /// Produces a ResourceDictionary from this themes config.
+        /// </summary>
+        public ResourceDictionary ToResourceDictionary()
+        {
+            var resourceDict = new ResourceDictionary();
+            // process color settings
+            foreach (var param in _colorParams) {
+                if (Config.ContainsKey(param)) {
+                    var value = Config[param];
+                    var brush = new BrushConverter().ConvertFromString(value);
+                    resourceDict.Add(param, brush);
+                }
+            }
+            return resourceDict;
+        }
+
+        /// <summary>
+        /// Clones this instance (same GUID)
+        /// </summary>
+        public Theme Clone()
+        {
+            var clone = new Theme {
+                Config = new Dictionary<string, string>(Config),
+                FilePath = FilePath,
+                Editable = Editable
+            };
+            return clone;
+        }
+        
+        /// <summary>
+        /// Returns a clone of this instance (with new GUID)
+        /// </summary>
+        public Theme Duplicate()
+        {
+            var clone = Clone();
+            clone.SetupNew();
+            clone.Editable = true;
+            clone.Save();
+            return clone;
+        }
+
+        /// <summary>
+        /// Copies config from another theme into this theme.
+        /// </summary>
+        /// <param name="theme">The theme.</param>
+        public void CopyFrom(Theme theme)
+        {
+            Config = theme.Config;
+            Editable = theme.Editable;
+            FilePath = theme.FilePath;
+        }
+    }
+}
---
+++ Else/Lib/ThemeManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.CodeDom;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Else.Properties;
+
+namespace Else.Lib
+{
+    public class ThemeManager
+    {
+        public class ThemeGuidAlreadyExists : Exception
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:System.Exception"/> class.
+            /// </summary>
+            public ThemeGuidAlreadyExists() {}
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:System.Exception"/> class with a specified error message.
+            /// </summary>
+            /// <param name="message">The message that describes the error. </param>
+            public ThemeGuidAlreadyExists(string message) : base(message) { }
+        }
+
+        /// <summary>
+        /// Loaded themes are stored here
+        /// </summary>
+        public BindingList<Theme> Themes = new BindingList<Theme>();
+
+        /// <summary>
+        /// The currently active theme.
+        /// </summary>
+        public Theme ActiveTheme;
+
+        
+        /// <summary>
+        /// Scans a directory for files with a .json extension, and attempts to load them as themes.
+        /// </summary>
+        public void ScanForThemes(string directory, bool isEditable)
+        {
+            foreach (var path in Directory.EnumerateFiles(directory).Where(s => s.EndsWith(".json"))) {
+                if (File.Exists(path)) {
+                    var theme = new Theme();
+                    theme.LoadFromPath(path);
+                    theme.Editable = isEditable;
+                    RegisterTheme(theme);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Register the theme, so it is available to parts of the application.
+        /// </summary>
+        /// <param name="theme">The theme.</param>
+        /// <exception cref="ThemeGuidAlreadyExists">The theme name is already registered</exception>
+        public void RegisterTheme(Theme theme)
+        {
+            if (Themes.Any(t => t.GUID == theme.GUID)) {
+                throw new ThemeGuidAlreadyExists(theme.GUID);
+            }
+            Themes.Add(theme);
+        }
+
+        /// <summary>
+        /// Unregister the theme.
+        /// </summary>
+        /// <param name="theme">The theme.</param>
+        public void UnregisterTheme(Theme theme)
+        {
+            Themes.Remove(theme);
+        }
+
+        /// <summary>
+        /// Get the configured theme (from settings), and apply it.
+        /// </summary>
+        public void ApplyThemeFromSettings()
+        {
+            try {
+                ApplyTheme(Settings.Default.Theme);
+            }
+            catch (InvalidOperationException) {
+                // configured theme does not exist, restore the default
+                var defaultTheme = Settings.Default.Properties["Theme"].DefaultValue as string;
+                try {
+                    ApplyTheme(defaultTheme);
+                }
+                catch {
+                    // erk, default theme does not exit
+                    // try and set any theme we have
+                    if (Themes.Any()) {
+                        ApplyTheme(Themes.First());
+                        SaveSettings();
+                        Debug.Print("Failed to set default theme, so we set {0} instead.", Themes.First().Name);
+                    }
+                    else {
+                        // otherwise run without a theme
+                        Debug.Print("No themes found at all");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Applys the named theme.
+        /// </summary>
+        public void ApplyTheme(string guid)
+        {
+            var theme = Themes.First(t => t.GUID == guid);
+            ApplyTheme(theme);
+            SaveSettings();
+        }
+
+        /// <summary>
+        /// Applys the theme by modifying App Resources.
+        /// </summary>
+        /// <remarks>This method is called heavily by the theme editor.</remarks>
+        /// <param name="theme">The theme.</param>
+        public void ApplyTheme(Theme theme)
+        {
+            // convert theme to ResourceDictionary
+            var themeResourceDictionary = theme.ToResourceDictionary();
+            
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            // need to remove the last theme resource dictionary
+            if (ActiveTheme != null) {
+                mergedDictionaries.RemoveAt(mergedDictionaries.Count - 1);
+            }
+            Application.Current.Resources.MergedDictionaries.Add(themeResourceDictionary);
+            
+            ActiveTheme = theme;
+        }
+
+        public void SaveSettings()
+        {
+            if (ActiveTheme != null) {
+                Settings.Default.Theme = ActiveTheme.GUID;
+                Settings.Default.Save();
+            }
+        }
+    }
+}
