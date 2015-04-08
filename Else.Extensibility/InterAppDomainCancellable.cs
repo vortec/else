de7ab254e5848a27ee8cc44ad4f145ff27c47226
jameshy <jameshy64@gmail.com>
2015-04-08 02:04:44 +0100

    Loading plugins in seperate AppDomain

---
+++ Else.Extensibility/InterAppDomainCancellable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Else.Extensibility
+{
+    public interface ITokenSource
+    {
+        CancellationToken Token { get; }
+    }
+
+    public class InterAppDomainCancellable : MarshalByRefObject, ITokenSource, IDisposable
+    {
+        private readonly CancellationTokenSource cts;
+
+        public InterAppDomainCancellable()
+        {
+            cts = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            cts.Dispose();
+        }
+
+        public void Cancel()
+        {
+            cts.Cancel();
+        }
+
+        CancellationToken ITokenSource.Token
+        {
+            get { return cts.Token; }
+        }
+    }
+}
---
+++ Else/Core/PathBasedAssemblyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Else.Core
+{
+    class PathBasedAssemblyResolver : MarshalByRefObject
+    {
+        public List<string> Paths = new List<string>();
+
+        public Assembly Resolve(object sender, ResolveEventArgs args)
+        {
+            Debug.Print("HERE");
+            var name = new AssemblyName(args.Name);
+            foreach (var path in Paths) {
+                var dllPath = Path.Combine(path, string.Format("{0}.dll", name.Name));
+                if (File.Exists(dllPath)) {
+                    return Assembly.LoadFrom(dllPath);
+                }
+                var exePath = Path.ChangeExtension(dllPath, "exe");
+                if (File.Exists(exePath)) {
+                    return Assembly.LoadFrom(exePath);
+                }
+            }
+            // not found
+            return null;
+        }
+    }
+}
