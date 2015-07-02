1d6f9ab70afb06ce3589525c29e1ed992c7df784
jameshy <jameshy64@gmail.com>
2015-07-03 00:17:52 +0100

    Move from python 3.4 to 3.5

--- PluginHost.Python/PythonPlugin.cpp
+++ PluginHost.Python/PythonPlugin.cpp
@@ -60,9 +60,15 @@ namespace PythonPluginLoader {
         Py_DECREF(customLibPath);
 
         // append our zipped python standard library
-        auto pythonLibZip = PyUnicode_FromString("C:\\Users\\James\\Repos\\Else\\Python\\python_stdlib.zip");
+        auto pythonLibZip = PyUnicode_FromString("C:\\Users\\James\\Repos\\Else\\PluginHost.Python\\lib\\python3-stdlib.zip");
         PyList_Insert(pathObject, 0, pythonLibZip);
         Py_DECREF(pythonLibZip);
+
+        // check if module is a VENV module, and add site-packages
+        /*const char* venv_site_packages = context->marshal_as<const char*>(Path::Combine(pluginDir, "Lib\\site-packages"));
+        PyList_Insert(pathObject, 0, PyUnicode_FromString(venv_site_packages));
+        Py_DECREF(venv_site_packages);*/
+        
 
         // setup our helper module
         self = new gcroot<PythonPlugin^>(this);
