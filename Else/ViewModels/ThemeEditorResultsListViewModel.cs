5fcac247e7ad4c89aa7628619e9fd80372edf103
jameshy <jameshy64@gmail.com>
2015-07-06 16:53:55 +0100

    Disable compiler warning

--- Else/ViewModels/ThemeEditorResultsListViewModel.cs
+++ Else/ViewModels/ThemeEditorResultsListViewModel.cs
@@ -34,6 +34,8 @@ namespace Else.ViewModels
 
         public RelayCommand PreviewKeyDown { get; set; }
         public int SelectedIndex { get; set; }
+        #pragma warning disable 67
         public event PropertyChangedEventHandler PropertyChanged;
+        #pragma warning restore 67
     }
 }
